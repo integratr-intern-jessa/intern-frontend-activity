9d7535a1e1048c39c5ec1cfb3334a03518aa83e4
Jessa Mae Aruta <jaruta.intern.integratr@outlook.com>
2023-03-31 16:37:12 +0800

    initial commit

---
+++ scrpit.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container"),
+login = document.querySelector(".login-link");
+      if(password.type =="password"){
+        
+      }
+
+login.addEventListener("click", ( )=>{
+  container.classList.remove("active");
+});
