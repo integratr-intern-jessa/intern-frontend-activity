faf4be76e06a0232808ee2e41d4083ef188d9c4b
Jessa Mae Aruta <jaruta.intern.integratr@outlook.com>
2023-04-11 22:36:39 +0800

    forEach

--- script.js
+++ script.js
@@ -1,65 +1,115 @@
-function validate()
-{
-  var fname = document.registration_form.fname;
-  var lname = document.registration_form.lname;
-  var uname = document.registration_form.uname;
-  if (fname.value.length <= 0)
-  {
-    alert("First Name is required");
-    fname.focus();
-    return false;
-  }
-  if (fname.value.length <= 0)
-  {
-    alert("Last Name is required");
-    lname.focus();
-    return false;
-  }
-  if (uname.value.length <= 0)
-  {
-    alert("User Name is required");
-    uname.focus();
-    return false;
-  }
-  return false;
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(checkInputs) {
+	checkInputs.preventDefault();
+
+	const inputs = form.querySelectorAll('input');
+	const inputValues = {};
+
+	inputs.forEach(function(input) {
+		inputValues[input.id] = input.value;
+	});
+	const required = ["firstname", "lastname", "username","password", "confirmpassword"];
+	let isinputfieldValid = true;
+
+	required.forEach(function(fieldName) {
+		const fieldValues = inputValues[fieldName];
+
+		if (!fieldValues || fieldValues.trim() === '') {
+			alert(`The ${fieldName} must be filled.`);
+			isinputfieldValid = false;
+		}
+	});
+
+	if (isinputfieldValid) {
+		const password = inputValues['password'];
+		const confirmpassword = inputValues['confirmpassword'];
+	
+		if (password !== confirmpassword) {
+		  alert('Password does not match.');
+		  isinputfieldValid = false;
+		}
+		else if (password.length < 14 || !password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{14,}$/)) {
+			alert('Password must contains: 14 characters, uppercase letter, lowercase letter, number, and symbol.');
+			isinputfieldValid = false;
+    }
+	  }
+	if (isinputfieldValid  ) {
+		form.submit();
+	}
+});
+
+
+var myInput = document.getElementById("password");
+var length = document.getElementById("length");
+var lowercase = document.getElementById("lowercase");
+var uppercase = document.getElementById("uppercase");
+var numbers = document.getElementById("numbers");
+var symbol = document.getElementById("symbol");
+
+myInput.onfocus =function(){
+	document.getElementById("validation").style.display="block";
 }
 
-function verifyPassword()
-{
-	var pass = document.getElementById("pass").value;
-	var password = document.getElementById("confirmpass").value;
-	const Uppercaseletters = /^(?=.*[A-Z]).*$/;
-	const Lowercaseletters = /^(?=.*[a-z]).*$/;
-	const Number = /^(?=.*[0-9]).*$/;
-	const Symbol = /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+myInput.onblur = function(){
+	document.getElementById("validation").style.display="none";
+}
 
-	if (pass.length < 12)
-	{
-		document.getElementById("message").innerHTML = "**Password length must be atleast 12 characters";
-		return false;
+myInput.onkeyup = function(){
+
+	var lowerCase = /[a-z]/g;
+
+	if(myInput.value.match(lowerCase)){
+		lowercase.classList.remove("invalid");
+		lowercase.classList.add("valid");
 	}
-	else if (!Uppercaseletters.test(password))
-	{
-		alert("**Password should contain uppercase letters.");
+	else {
+		lowercase.classList.remove("valid");
+		lowercase.classList.add("invalid");
 	}
-	else if (!Lowercaseletters.test(password))
-	{
-		alert("**Password should contain lowercase letters.");
+
+	var upperCase = /[A-Z]/g;
+
+	if(myInput.value.match(upperCase)){
+		uppercase.classList.remove("invalid");
+		uppercase.classList.add("valid");
 	}
-	else if (!Number.test(password))
-	{
-		alert("**Password should contain a numbers.");
+	else {
+		uppercase.classList.remove("valid");
+		uppercase.classList.add("invalid");
 	}
-	else if (!Symbol.test(password))
-	{
-		alert("**Password should contain symbols.");
+
+	var Numbers = /[0-9]/g;
+
+	if(myInput.value.match(Numbers)){
+		numbers.classList.remove("invalid");
+		numbers.classList.add("valid");
 	}
-	else if (password == "")
-	{
-		alert("**Please make sure your passwords match.");
+	else {
+		numbers.classList.remove("valid");
+		numbers.classList.add("invalid");
 	}
-	else 
-	{
-		alert("**Successfully regsitered!")
+
+	var Symbols = /[!@#$%^&*]/g;
+
+	if(myInput.value.match(Symbols)){
+		symbol.classList.remove("invalid");
+		symbol.classList.add("valid");
 	}
+	else {
+		symbol.classList.remove("valid");
+		symbol.classList.add("invalid");
+	}
+
+
+	if(myInput.value.length>=14){
+		length.classList.remove("invalid");
+		length.classList.add("valid");
+	}
+	else {
+		length.classList.remove("valid");
+		length.classList.remove("invalid");
+	}
+
 }
